5529d9ad17cff4811c90bfe0d1f40e1bdf7de7ea
serafim.eth <serafimcloud@gmail.com>
2025-01-31 21:48:56 +0300

    remove: product hunt
    Update providers.tsx

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,14 +1,12 @@
 "use client"
 
 import { useEffect } from "react"
-import { usePathname } from "next/navigation"
 
 import { ClerkProvider } from "@clerk/nextjs"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { CommandMenu } from "@/components/ui/command-menu"
-import { ProductHuntDialog } from "@/components/ui/product-hunt-dialog"
 
 import { initAmplitude } from "@/lib/amplitude"
 
@@ -27,7 +25,6 @@ export function AppProviders({
     <QueryClientProvider client={queryClient}>
       <ClerkProvider>
         <CommandMenu />
-        <ProductHuntDialog />
         <SpeedInsights />
         {children}
       </ClerkProvider>
