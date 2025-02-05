248fb52e0bb2b0283c8b6f33086de2bc68ef041c
Alex Danilowicz <alex.danilowicz@gmail.com>
2025-02-05 16:34:08 -0600

    fix: Fix Magic Patterns logo size to be 22 by 22 (#102)
    * fix: Fix Magic Patterns logo size to be 22 by 22
    
    * Update magic-patterns.tsx
    
    ---------
    
    Co-authored-by: serafim.eth <serafimcloud@gmail.com>

--- apps/web/components/icons/magic-patterns.tsx
+++ apps/web/components/icons/magic-patterns.tsx
@@ -1,7 +1,6 @@
 export const MagicPatternsLogo = ({ className }: { className?: string }) => (
   <svg
-    width="85"
-    height="85"
+    className={className}
     viewBox="0 0 85 85"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
