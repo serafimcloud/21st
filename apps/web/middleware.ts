770ea043bd482abc33d193091ad3901268183b04
serafim.eth <serafimcloud@gmail.com>
2025-02-08 12:51:51 +0300

    fix: publishing

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+import type { NextRequest, NextFetchEvent } from "next/server"
 
-// Make sure that the `/api/webhooks(.*)` route is not protected here
-export default clerkMiddleware()
-
-export function middleware(request: NextRequest) {
+export default function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+) {
   if (request.nextUrl.pathname.startsWith("/api/")) {
     const requestHeaders = new Headers(request.headers)
     requestHeaders.set("x-internal-token", process.env.INTERNAL_API_SECRET!)
@@ -16,13 +16,13 @@ export function middleware(request: NextRequest) {
       },
     })
   }
+
+  return clerkMiddleware(request, event)
 }
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    "/((?!.+\\.[\\w]+$|_next).*)", 
     "/(api|trpc)(.*)",
   ],
 }
