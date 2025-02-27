9f8a1c1c9ce954e59c96aa5c85c7559c47aab543
serafim.eth <serafimcloud@gmail.com>
2025-02-27 18:18:57 +0300

    fix: build

--- apps/web/components/ui/header.client.tsx
+++ apps/web/components/ui/header.client.tsx
@@ -257,7 +257,7 @@ export function Header({
                   >
                     Edit Profile
                   </DropdownMenuItem>
-                  <DropdownMenuItem
+                  {/* <DropdownMenuItem
                     className="text-sm px-3 py-2 cursor-pointer flex items-center justify-between"
                     onSelect={() => router.push("/settings")}
                   >
@@ -270,7 +270,7 @@ export function Header({
                   >
                     Subscription
                     <Icons.creditCard className="h-4 w-4" />
-                  </DropdownMenuItem>
+                  </DropdownMenuItem> */}
                 </div>
 
                 <div className="border-t border-border p-1">
