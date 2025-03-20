39e120c14012038f185c580c2280007e514e2184
serafim <serafimcloud@gmail.com>
2025-03-20 07:15:35 +0300

    Update footer.tsx

--- apps/web/components/ui/footer.tsx
+++ apps/web/components/ui/footer.tsx
@@ -14,22 +14,8 @@ export function Footer({ className, isOpenSource = true }: FooterProps) {
       <div className="container mx-auto px-[var(--container-x-padding)] max-w-[3680px] [--container-x-padding:20px] min-720:[--container-x-padding:24px] min-1280:[--container-x-padding:32px] min-1536:[--container-x-padding:80px]">
         <div className="flex flex-col h-auto py-4 gap-4 text-center md:flex-row md:h-14 md:items-center md:justify-between md:py-0">
           <div className="flex flex-wrap items-center justify-center gap-1 text-sm text-muted-foreground md:flex-nowrap md:justify-start">
-            Built by{" "}
-            <Link
-              href="https://x.com/serafimcloud"
-              target="_blank"
-              className="font-medium underline-offset-4 hover:underline"
-            >
-              serafim
-            </Link>{" "}
-            &{" "}
-            <Link
-              href="https://github.com/serafimcloud/21st/graphs/contributors"
-              target="_blank"
-              className="font-medium underline-offset-4 hover:underline"
-            >
-              friends
-            </Link>
+            21st Labs Inc.
+          
             {isOpenSource && (
               <>
                 . The source code is available on{" "}
