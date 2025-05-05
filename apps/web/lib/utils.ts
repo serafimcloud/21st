34bac46c5fd3fbb552796061e46a3801c6248925
Danverr <ddmaryakhin@edu.hse.ru>
2025-05-05 11:26:45 +0300

    payouts page fix

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -61,3 +61,10 @@ export function formatPrice(price: number) {
     maximumFractionDigits: 2,
   }).format(price)
 }
+
+export function formatK(num: number, toFixed = 1): string {
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k"
+  }
+  return num.toFixed(toFixed)
+}
