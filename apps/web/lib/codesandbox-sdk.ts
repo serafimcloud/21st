ba870e8516cd161fcdc69e00580f793081306317
Serge Bunas <Sergeibynas@gmail.com>
2025-05-16 21:53:56 +0400

    update codesandbox template ID for 21st-vite

--- apps/web/lib/codesandbox-sdk.ts
+++ apps/web/lib/codesandbox-sdk.ts
@@ -5,7 +5,7 @@ export const codesandboxSdk = new CodeSandbox(process.env.CSB_API_KEY!)
 export const DEFAULT_TEMPLATE = "21st-vite"
 
 export const TEMPLATES = {
-  "21st-vite": "5jxc5h", // "d5t2cg",
+  "21st-vite": "kwk42j", // "d5t2cg",
 }
 
 export const DEFAULT_HIBERNATION_TIMEOUT = 60
