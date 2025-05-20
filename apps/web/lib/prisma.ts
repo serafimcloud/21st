ecbdf6a0605907babe24fbdcf70e228ff7107da7
Daniil Maryakhin <47456007+Danverr@users.noreply.github.com>
2025-05-20 21:11:04 +0400

    prisma conn test

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@/prisma/client"
 
-const prisma = new PrismaClient()
+console.log("DATABASE_URL", process.env.DATABASE_URL)
+console.log("DIRECT_DATABASE_URL", process.env.DIRECT_DATABASE_URL)
 
-export default prisma
+let prisma: PrismaClient
+try {
+  prisma = new PrismaClient()
+} catch (error) {
+  console.error("Failed to connect to database", JSON.stringify(error, null, 2))
+}
+
+export default prisma!
