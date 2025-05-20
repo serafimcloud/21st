035db891126ef4e2aa09143f40b88a1980fae6dc
Daniil Maryakhin <47456007+Danverr@users.noreply.github.com>
2025-05-20 20:07:22 +0400

    nextjs-monorepo-workaround-plugin

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,5 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -16,6 +18,8 @@ const nextConfig = {
         ...config.resolve.fallback,
         fs: false,
       }
+    } else {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
     }
     return config
   },
